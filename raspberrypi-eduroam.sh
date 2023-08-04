4301a96e4a7b1d1fa2359a0ae50ea4e730763711
biu1 <lptwomey@ucdavis.edu>
2023-08-04 12:51:45 -0700

    Fixed and added functionality to raspberrypi-eduroam.sh

--- raspberrypi-eduroam.sh
+++ raspberrypi-eduroam.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 #By Liam Twomey, based on the UCSC help article https://inrg.soe.ucsc.edu/howto-connect-raspberry-to-eduroam/
-# Flag Parsing
+
 PrintUsage(){
 	printf "Usage:
-	-i: Accepts user's Kerberos password as argument.
+	-i: Accepts user's Kerberos password as argument, and adds an 'eduroam' shortcut for this command to ~/.bashrc and ~/.zshrc if they exist.
 	-h: Prints this usage info. Refer to https://inrg.soe.ucsc.edu/howto-connect-raspberry-to-eduroam/ for more info 
-	All other parameters accepted as bash <read> parameters."
+	All other parameters accepted as bash <read> parameters.
+	This program changes:
+		/etc/network/interfaces,
+		/etc/wpa_supplicant/wpa_supplicant.conf"
 	exit 1
 }
-
+IfdownShutdown(){
+	sudo ifdown wlan0
+	sudo ifdown eth0
+	sudo killall wpa-supplicant
+}
 EditFiles(){
-	ifdown wlan0
-	ifdown eth0
-	killall wpa-supplicant
- 
+	#Shutdown hardware and processes
+	IfdownShutdown
 	# Update wifi config to check wpa_supplicant.conf for config settings
-	echo -e "# Changes made by Raspberry Pi Eduroam Configuration script. 
+	sudo echo -e "# Changes made by Raspberry Pi Eduroam Configuration script. 
 	allow-hotplug wlan0
 	iface wlan0 inet manual
 	\twpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
 	iface wlan0 inet dhcp" >> $interfacefile 
 
 	# Update settings in wpa_supplicant.conf
-	echo -e "# Changes made by the Raspberry Pi Eduroam Configuration script.  
+	sudo echo -e "# Changes made by the Raspberry Pi Eduroam Configuration script.  
 	ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 	update_config=1
 	country=$input_loc
@@ -36,19 +41,34 @@ EditFiles(){
 	}" >> $conffile && 
 	printf "\033[32mSettings changed.\033[0m\nTo view changes, visit $conffile. Automated changes were also made to $interfacefile. Run script with the -i flag to verify successful connection.\n" 
 }
+
+InitEduroam(){
+	IfdownShutdowni
+	initCmd='alias eduroam="sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf &"'
+	rcFiles=('$HOME/.bashrc' '$HOME/.zshrc')
+	#Add 'eduroam' command to existing shell rc files.
+	for rcFile in $rcFiles; do	
+		if [ -f "$rcfile"]; then #&& [! grep "$initCmd" "$rcFile"]; then
+			echo "$initCmd" >> "$rcFile"
+		fi
+	done
+	sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf &
+}
+
 # Process flags
-while getopts 'u:p:l:h' flag; do
+while getopts 'ih' flag; do
   case "${flag}" in
-    i) wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf | grep successful;; 
+    i) InitEduroam; exit;; 
     h) PrintUsage; exit;;
     \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
     :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
     *  ) echo "Unimplemented option: -$option" >&2; exit 1;;
   esac
 done
+
 #Source Files
-interfacefile= /etc/network/interfaces
-conffile= /etc/wpa_supplicant/wpa_supplicant.conf
+interfacefile="/etc/network/interfaces"
+conffile="/etc/wpa_supplicant/wpa_supplicant.conf"
 # Obtain user info
 read -p "Kerberos/Eduroam username:" input_username
 read -p "Kerberos/Eduroam password:" input_passwd
